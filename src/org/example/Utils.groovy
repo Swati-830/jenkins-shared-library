606744152a2030f71531c56bf6de842377a2c889
Swati-830 <swatijaiswal90090@gmail.com>
2026-02-17 13:44:05 +0530

    Initial shared library

---
+++ src/org/example/Utils.groovy
@@ -0,0 +1,15 @@
+package org.example
+
+class Utils implements Serializable {
+
+    def steps
+
+    Utils(steps) {
+        this.steps = steps
+    }
+
+    def printMessage(msg) {
+        steps.echo "Message: ${msg}"
+    }
+}
+
